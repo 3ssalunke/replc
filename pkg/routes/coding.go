cbe79d04d34047cb196c4c949b9a8f55e3c5b46e
Suraj Salunke <surajsalunke1276@gmail.com>
2024-03-12 21:56:20 +0530

    120324

--- pkg/routes/coding.go
+++ pkg/routes/coding.go
@@ -1,9 +1,6 @@
 package routes
 
 import (
-	"os"
-	"path/filepath"
-
 	"github.com/3ssalunke/replc/pkg/controller"
 	"github.com/3ssalunke/replc/templates"
 	"github.com/labstack/echo/v4"
@@ -18,23 +15,23 @@ type (
 func (c *coding) Get(ctx echo.Context) error {
 	replId := ctx.Param("replcid")
 
-	k8s, err := controller.NewK8S(c.Container.Config.K8S.Kubeconfigpath)
+	_, err := controller.NewK8S(c.Container.Config.K8S.Kubeconfigpath)
 	if err != nil {
 		return c.Fail(err, "failed to load kube configs")
 	}
 
-	// Get the current directory
-	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		return c.Fail(err, "failed to get current directory")
-	}
+	// // Get the current directory
+	// dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	// if err != nil {
+	// 	return c.Fail(err, "failed to get current directory")
+	// }
 
-	orchestratorYamlPath := filepath.Join(dir, "../orchestrator/service.yaml")
+	// orchestratorYamlPath := filepath.Join(dir, "../orchestrator/service.yaml")
 
-	err = c.Controller.CreateK8sResources(k8s, orchestratorYamlPath, replId)
-	if err != nil {
-		return c.Fail(err, "failed to load kube configs")
-	}
+	// err = c.Controller.CreateK8sResources(k8s, orchestratorYamlPath, replId)
+	// if err != nil {
+	// 	return c.Fail(err, "failed to load kube configs")
+	// }
 
 	page := controller.NewPage(ctx)
 	page.Layout = templates.LayoutMain
